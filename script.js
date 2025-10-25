06c241a141c8f79d885a5fb8b935f97509f3d9e2
deoalphaa <deoalph@gmail.com>
2025-10-25 10:33:04 +0800

    add browser logo

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 feather.replace()
 
 
-/*
-custom element, liat dri yutub :p
 
-class logo_money extends HTMLElement{
-    connectedCallback(){
-        this.innerHTML = `
-            <h1>
-                <b>Money Management</b>
-                <i data-feather="moon" id="dark"></i>
-            </h1>
-        `;
-    }
-}
-customElements.define("logo-money", logo_money);
-*/
+// custom element, liat dri yutub :p
+
+// class logo_money extends HTMLElement{
+//     connectedCallback(){
+//         this.innerHTML = `
+//             <h1>
+//                 <b>Money Management</b>
+//                 <i data-feather="moon" id="dark"></i>
+//             </h1>
+//         `;
+//     }
+// }
+// customElements.define("logo-money", logo_money);
