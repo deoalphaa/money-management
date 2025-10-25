2e776af7889a73d45c2cd031c9a9f775b24430f6
Makanleleayam <charissurijakko1@gmail.com>
2025-10-25 08:41:24 +0700

    Added new folder: <pages>, global CSS variables

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 feather.replace()
+
+
+/*
+custom element, liat dri yutub :p
+
+class logo_money extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+            <h1>
+                <b>Money Management</b>
+                <i data-feather="moon" id="dark"></i>
+            </h1>
+        `;
+    }
+}
+customElements.define("logo-money", logo_money);
+*/
